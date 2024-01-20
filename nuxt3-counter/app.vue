407607893c5948cf554bf51f08eb025b61198beb
takyafumin <takyafumin@gmail.com>
2024-01-20 20:33:22 +0900

    Section9, composableにしない実装

--- nuxt3-counter/app.vue
+++ nuxt3-counter/app.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts"></script>
+
 <template>
   <div>
-    <h1>Hello World</h1>
+    <Counter />
   </div>
 </template>
+
+<style scoped></style>
---
+++ nuxt3-counter/components/Counter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const count = ref<number>(0);
+
+const increment = () => count.value++;
+const decrement = () => {
+  if (count.value <= 0) {
+    return;
+  }
+  count.value--;
+};
+const reset = () => (count.value = 0);
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    {{ count }}
+    <div>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <button @click="reset">reset</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
