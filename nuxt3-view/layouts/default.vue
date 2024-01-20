4dffea232ccc4320f8835d71759406117f8f51e6
takyafumin <takyafumin@gmail.com>
2024-01-20 14:24:30 +0900

    Section5, Add default layout

---
+++ nuxt3-view/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <slot />
+    <footer>&copy; takyafumin</footer>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  color: red;
+}
+
+footer {
+  background-color: orange;
+  color: white;
+  text-align: center;
+}
+</style>
