763b12ae224e2f06ea2f4e770c648834deae1f16
takyafumin <takyafumin@gmail.com>
2024-01-20 15:24:06 +0900

    Section6, Add API call by useFetch()

--- nuxt3-async/app.vue
+++ nuxt3-async/app.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import type { User } from "~/types/user";
+
+const { data: users } = await useFetch<Array<User>>(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
 <template>
   <div>
-    <NuxtPage />
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
