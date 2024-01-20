9ef60bddf3ff87cc41df24433870c93cf8a81c23
takyafumin <takyafumin@gmail.com>
2024-01-20 14:35:21 +0900

    Section5, Error Page

---
+++ nuxt3-view/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const error = useError();
+console.log(error);
+</script>
+
+<template>
+  <div v-if="error.statusCode === 404">
+    <h1>Error!</h1>
+    <p>{{ error.message }}</p>
+  </div>
+</template>
+
+<style scoped></style>
