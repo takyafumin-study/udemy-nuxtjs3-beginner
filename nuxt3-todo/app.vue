ac3b0bfe492fd44bfffe5485170284afc2ce9b67
takyafumin <takyafumin@gmail.com>
2024-01-22 13:57:34 +0900

    Section10, Add 削除機能

--- nuxt3-todo/app.vue
+++ nuxt3-todo/app.vue
@@ -3,12 +3,30 @@ const tasks = useCookie<Array<string>>("tasks", {
   default: () => [],
 });
 
+/**
+ * Add a new task
+ */
 const newTask = ref("");
 const addTask = () => {
   if (newTask.value.length >= 1) {
     tasks.value.push(newTask.value);
   }
   newTask.value = "";
+};
+
+/**
+ * Delete a task
+ * @param index - index of task to delete
+ */
+const deleteTask = (index: number) => {
+  tasks.value.splice(index, 1);
+};
+
+/**
+ * Clear all tasks
+ */
+const clearTasks = () => {
+  tasks.value = [];
 };
 </script>
 
@@ -20,12 +38,12 @@ const addTask = () => {
       <button>Add</button>
     </form>
     <ul>
-      <li v-for="task in tasks" :key="task">
+      <li v-for="(task, index) in tasks" :key="index">
         <span>{{ task }}</span>
-        <button>Delete</button>
+        <button @click="() => deleteTask(index)">Delete</button>
       </li>
     </ul>
-    <button>Clear</button>
+    <button @click="clearTasks">Clear</button>
   </div>
 </template>
 
