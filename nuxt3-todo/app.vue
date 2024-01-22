3540f6824a904beb8c028084a1ef7bfe480eb675
takyafumin <takyafumin@gmail.com>
2024-01-22 13:52:51 +0900

    Section10, Add タスク追加、表示機能実装

--- nuxt3-todo/app.vue
+++ nuxt3-todo/app.vue
@@ -1,8 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tasks = useCookie<Array<string>>("tasks", {
+  default: () => [],
+});
+
+const newTask = ref("");
+const addTask = () => {
+  if (newTask.value.length >= 1) {
+    tasks.value.push(newTask.value);
+  }
+  newTask.value = "";
+};
+</script>
 
 <template>
   <div>
-    <p>Hello world!</p>
+    <h1>To DO</h1>
+    <form @submit.prevent="addTask">
+      <input v-model="newTask" name="newTask" autocomplete="off" />
+      <button>Add</button>
+    </form>
+    <ul>
+      <li v-for="task in tasks" :key="task">
+        <span>{{ task }}</span>
+        <button>Delete</button>
+      </li>
+    </ul>
+    <button>Clear</button>
   </div>
 </template>
 
